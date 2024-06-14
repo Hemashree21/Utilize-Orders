256500f8b918121b5bc9e0226b31d9e04fb7e321
Hack2Health <hemashree21.msh@gmail.com>
2024-06-14 20:46:14 +0530

    Added Protected Routes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Auth from './Auth'
 import Home from './Home'
 import CreateOrder from './CreateOrder'
+import ProtectedRoute from './ProtectedRoute'
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Auth/>}/>
-      <Route path='/home' element={<Home/>}/>
-      <Route path='/createorder' element={<CreateOrder/>}/>
-      <Route path='/editorder/:id' element={<CreateOrder/>}/>
+      <Route path='/home' element={<ProtectedRoute element={<Home/>}/>}/>
+      <Route path='/createorder' element={<ProtectedRoute element={<CreateOrder/>}/>}/>
+      <Route path='/editorder/:id' element={<ProtectedRoute element={<CreateOrder/>}/>}/>
     </Routes>
     </BrowserRouter>
   )
